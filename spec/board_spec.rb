c1d60c223d089cafcdb01d7defa8640f4a86cff4
Victor Sanchez <vsanantone@gmail.com>
2023-07-25 14:17:59 -0500

    test:add place method test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -73,5 +73,18 @@ RSpec.describe Board do
     end
   end
 
+  describe "#place" do
+    it "places a ship on the grid" do
+      @board.place(@cruiser, ["A1", "A2", "A3"])
+      cell_1 = @board.cells["A1"]
+      cell_2 = @board.cells["A2"]
+      cell_3 = @board.cells["A3"]
 
+      expect(cell_1.ship).to eq @cruiser
+      expect(cell_2.ship).to eq @cruiser
+      expect(cell_3.ship).to eq @cruiser 
+
+      expect(cell_3.ship).to eq cell_2.ship
+    end
+  end
 end
