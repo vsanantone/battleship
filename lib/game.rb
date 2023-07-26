83172488d959cb958b10ed9f38c632cf06fa6052
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-07-26 15:07:14 -0600

    feat: Add start_game method to game class and runner file

--- lib/game.rb
+++ lib/game.rb
@@ -5,5 +5,22 @@ class Game
     puts "#{welcome}\n#{play}" 
     return "#{welcome}\n#{play}" 
   end
+
+  def start_game
+    intro
+    choice = gets.chomp.downcase
+    
+    loop do
+      if choice == "p"
+        puts "Preapre your Battleships!"
+      elsif choice == "q"
+        puts "Better swim to shore, the sharks are hungry!"
+        break
+      else
+        puts "That's not what we asked you..."
+      end
+      break
+    end
+  end
 end
 
